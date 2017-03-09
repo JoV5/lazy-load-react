43b9b8f8f034765326f7df3eb45cbbb3f5a497be
JoV5 <360870202@qq.com>
2017-03-09 14:42:24 +0800

    fix hot reloading didn't work

--- dist/lazy-load-react.js
+++ dist/lazy-load-react.js
@@ -3,9 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.lazyme = exports.importLazy = exports.LazilyLoadFactory = exports.LazilyLoad = undefined;
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+exports.lazyme = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -24,115 +22,15 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 */
 
 
-var LazilyLoad = exports.LazilyLoad = function (_React$Component) {
-  _inherits(LazilyLoad, _React$Component);
-
-  function LazilyLoad() {
-    _classCallCheck(this, LazilyLoad);
-
-    var _this = _possibleConstructorReturn(this, (LazilyLoad.__proto__ || Object.getPrototypeOf(LazilyLoad)).apply(this, arguments));
-
-    _this.state = {
-      isLoaded: false
-    };
-    return _this;
-  }
-
-  _createClass(LazilyLoad, [{
-    key: 'componentWillMount',
-    value: function componentWillMount() {
-      this.load(this.props);
-    }
-  }, {
-    key: 'componentDidMount',
-    value: function componentDidMount() {
-      this._isMounted = true;
-    }
-  }, {
-    key: 'componentWillReceiveProps',
-    value: function componentWillReceiveProps(next) {
-      if (next.modules === this.props.modules) return null;
-      this.load(next);
-    }
-  }, {
-    key: 'componentWillUnmount',
-    value: function componentWillUnmount() {
-      this._isMounted = false;
-    }
-  }, {
-    key: 'load',
-    value: function load(props) {
-      var _this2 = this;
-
-      this.setState({
-        isLoaded: false
-      });
-
-      var modules = props.modules;
-
-      var keys = Object.keys(modules);
-
-      Promise.all(keys.map(function (key) {
-        return modules[key]();
-      })).then(function (values) {
-        return keys.reduce(function (agg, key, index) {
-          agg[key] = values[index];
-          return agg;
-        }, {});
-      }).then(function (result) {
-        if (!_this2._isMounted) return null;
-        _this2.setState({ modules: result, isLoaded: true });
-      });
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      if (!this.state.isLoaded) return null;
-      return _react2.default.Children.only(this.props.children(this.state.modules));
-    }
-  }]);
-
-  return LazilyLoad;
-}(_react2.default.Component);
-
-LazilyLoad.propTypes = {
-  children: _react2.default.PropTypes.func.isRequired
-};
-
-var LazilyLoadFactory = exports.LazilyLoadFactory = function LazilyLoadFactory(Component, modules) {
-  return function (props) {
-    return _react2.default.createElement(
-      LazilyLoad,
-      { modules: modules },
-      function (mods) {
-        return _react2.default.createElement(Component, _extends({}, mods, props));
-      }
-    );
-  };
-};
-
-var importLazy = exports.importLazy = function importLazy(promise) {
-  return promise.then(function (result) {
-    return result.default;
-  });
-};
-
-/*export const lazyme = (getModule) => (props) =>
- <LazilyLoad modules={{
- Module: () => importLazy(getModule())
- }}>
- {({Module}) => <Module {...props}/>}
- </LazilyLoad>;*/
-
 var lazyme = exports.lazyme = function lazyme(getModule) {
 
-  return function (_React$Component2) {
-    _inherits(LazyComponent, _React$Component2);
+  return function (_React$Component) {
+    _inherits(LazyComponent, _React$Component);
 
     function LazyComponent() {
       var _ref;
 
-      var _temp, _this3, _ret;
+      var _temp, _this, _ret;
 
       _classCallCheck(this, LazyComponent);
 
@@ -140,9 +38,9 @@ var lazyme = exports.lazyme = function lazyme(getModule) {
         args[_key] = arguments[_key];
       }
 
-      return _ret = (_temp = (_this3 = _possibleConstructorReturn(this, (_ref = LazyComponent.__proto__ || Object.getPrototypeOf(LazyComponent)).call.apply(_ref, [this].concat(args))), _this3), _this3.state = {
+      return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = LazyComponent.__proto__ || Object.getPrototypeOf(LazyComponent)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
         Module: undefined
-      }, _temp), _possibleConstructorReturn(_this3, _ret);
+      }, _temp), _possibleConstructorReturn(_this, _ret);
     }
 
     _createClass(LazyComponent, [{
@@ -156,6 +54,11 @@ var lazyme = exports.lazyme = function lazyme(getModule) {
         this._isMounted = true;
       }
     }, {
+      key: 'componentWillReceiveProps',
+      value: function componentWillReceiveProps() {
+        this.load();
+      }
+    }, {
       key: 'componentWillUnmount',
       value: function componentWillUnmount() {
         this._isMounted = false;
@@ -163,11 +66,11 @@ var lazyme = exports.lazyme = function lazyme(getModule) {
     }, {
       key: 'load',
       value: function load() {
-        var _this4 = this;
+        var _this2 = this;
 
         getModule().then(function (result) {
-          if (!_this4._isMounted) return null;
-          _this4.setState({ Module: result.default });
+          if (!_this2._isMounted) return null;
+          _this2.setState({ Module: result.default });
         });
       }
     }, {
